881f6181e07e0882b512c02ac243d79d5fabafae
ConnectedSystems <takuyai@gmail.com>
2020-07-19 23:36:46 +1000

    Export new helper func

--- src/Agtor.jl
+++ src/Agtor.jl
@@ -28,6 +28,6 @@ AgUnion = Union{Int64, Float64, Agtor.AgParameter}
 export AgComponent, AgParameter, Infrastructure, Irrigation, Pump
 export WaterSource, Crop, FarmField, CropField, FarmZone, Manager, Climate, Basin
 export load_yaml, generate_params, create, run_timestep, subtotal_costs, total_costs
-export collect_results, min_max, param_values, AgUnion, add_prefix!, @def
+export collect_results, min_max, param_values, param_info, AgUnion, add_prefix!, @def
 
 end # module
