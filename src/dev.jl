593413b8e372c9bd6566952d98e64f5451ef4f67
ConnectedSystems <takuyai@gmail.com>
2020-07-19 10:19:11 +1000

    Continued development of main dev code

--- src/dev.jl
+++ src/dev.jl
@@ -3,11 +3,16 @@ import Agtor
 
 using Profile, BenchmarkTools, OwnTime, Logging
 
-using JLD, HDF5
+using JLD, HDF5, CSV
 using Dates
 using DataStructures
 using DataFrames
 using Agtor
+using Flatten
+using Infiltrator
+
+# Assumes we are in top-level project dir
+# julia --project=.
 
 # Start with the environment variable set for multi-threading
 # $ JULIA_NUM_THREADS=4 ./julia
@@ -15,85 +20,22 @@ using Agtor
 # Windows:
 # $ set JULIA_NUM_THREADS=4 && julia dev.jl
 
+
 # Start month/day of growing season
 # Represents when water allocations start getting announced.
 const gs_start = (5, 1)
 
-function setup_zone(data_dir::String="../test/data/")
-    climate_dir::String = "$(data_dir)climate/"  
-    tgt::String = climate_dir * "farm_climate_data.csv"
+function setup_zone(data_dir::String="test/data/")
+    zone_spec_dir::String = "$(data_dir)zones/"
+    zone_specs::Dict{String, Dict} = load_yaml(zone_spec_dir)
 
-    use_threads = Threads.nthreads() > 1
-    data = CSV.read(tgt, threaded=use_threads, dateformat="dd-mm-YYYY")
-    climate_data::Climate = Climate(data)
-
-    crop_dir::String = "$(data_dir)crops/"
-    crop_data::Dict{String, Dict} = load_yaml(crop_dir)
-    crop_rotation::Array{Crop} = Crop[create(Crop, data, climate_data.time_steps[1]) for data in values(crop_data)]
-
-    irrig_dir = "$(data_dir)irrigations/"
-    irrig_specs::Dict{String, Dict} = load_yaml(irrig_dir)
-    irrig = nothing
-    # Only 1 irrigation type for now.
-    for v in values(irrig_specs)
-        # `implemented` can be set at the field or zone level...
-        irrig = create(Irrigation, v)
-    end
-
-    water_spec_dir::String = "$(data_dir)water_sources/"
-    pump_spec_dir::String = "$(data_dir)pumps/"
-    water_specs::Dict{String, Dict} = load_yaml(water_spec_dir)
-    pump_specs::Dict{String, Dict} = load_yaml(pump_spec_dir)
-    w_specs::Array{WaterSource} = []
-    for (k, v) in water_specs
-        # TODO: This should be part of the spec...
-        if v["name"] == "groundwater"
-            ini_head = 25.0
-            allocation = 50.0
-        else
-            ini_head = 0.0
-            allocation = 225.0
-        end
-
-        pump_name = v["name"]
-        v["pump"] = create(Pump, pump_specs[pump_name])
-        v["head"] = ini_head  # convert to metre type
-        v["allocation"] = allocation
-        
-        ws::WaterSource = create(WaterSource, v)
-        push!(w_specs, ws)
-    end
-
-    f1_spec::Dict{Symbol, Any} = Dict(
-        :name => "field1",
-        :total_area_ha => 100.0,
-        :irrigation => irrig,
-        :crop => crop_rotation[1],
-        :crop_choices => crop_rotation,
-        :crop_rotation => crop_rotation,
-        :soil_TAW => 100.0,
-        :soil_SWD => 20.0
-    )
-    f2_spec::Dict{Symbol, Any} = copy(f1_spec)
-    f2_spec[:name] = "field2"
-    f2_spec[:total_area_ha] = 90.0
-    field1::CropField = CropField(; f1_spec...)
-    field2::CropField = CropField(; f2_spec...)
-
-    zone_spec::Dict{Symbol, Any} = Dict(
-        :name => "Zone_1",
-        :climate => climate_data,
-        :fields => CropField[field1, field2],
-        :water_sources => w_specs
-    )
-
-    z1::FarmZone = FarmZone(; zone_spec...)
-
-    return z1, w_specs
+    return [create(FarmZone, z_spec) for (z_name, z_spec) in zone_specs]
 end
 
-function test_short_run(data_dir::String="../test/data/")::Tuple{DataFrame,Dict{Any,Any}}
-    z1, (deeplead, channel_water) = setup_zone(data_dir)
+function test_short_run(data_dir::String="test/data/")::Tuple{DataFrame,Dict{Any,Any}}
+    # z1, (deeplead, channel_water) = setup_zone(data_dir)
+    zones = setup_zone(data_dir)
+    z1 = zones[1]
 
     farmer = Manager("test")
     time_sequence = z1.climate.time_steps
@@ -143,8 +85,6 @@ jldopen("test.jld", "w") do file
     g = g_create(file, "zone_1") # create a group
     g["zone_results"] = zone_results
     g["field_results"] = field_results
-    # write(file, "zone_results", zone_results)
-    # write(file, "field_results", field_results)
 end
 
 zone_results = jldopen("test.jld", "r") do file
