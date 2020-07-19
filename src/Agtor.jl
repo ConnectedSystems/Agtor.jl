51b637383add0713e760877b16968e8778d49d19
ConnectedSystems <takuyai@gmail.com>
2020-07-19 10:19:29 +1000

    Added use of AgParams

--- src/Agtor.jl
+++ src/Agtor.jl
@@ -21,10 +21,13 @@ include("Field.jl")
 include("Zone.jl")
 include("Manager.jl")
 include("Climate.jl")
+include("Basin.jl")
 
-export AgComponent, Infrastructure, Irrigation, Pump
-export WaterSource, Crop, FarmField, CropField, FarmZone, Manager, Climate
+AgUnion = Union{Int64, Float64, Agtor.AgParameter}
+
+export AgComponent, AgParameter, Infrastructure, Irrigation, Pump
+export WaterSource, Crop, FarmField, CropField, FarmZone, Manager, Climate, Basin
 export load_yaml, generate_params, create, run_timestep, subtotal_costs, total_costs
-export collect_results, min_max
+export collect_results, min_max, param_values, AgUnion, add_prefix!, @def
 
 end # module
