0ee918975dbd5f98cf340e4821a5991d1f331d9b
ConnectedSystems <takuyai@gmail.com>
2020-07-19 10:20:02 +1000

    Added Basin object to house collection of zones

---
+++ src/Basin.jl
@@ -0,0 +1,33 @@
+import Agtor: Climate, FarmZone
+
+mutable struct Basin <: AgComponent
+    climate_options::CategoricalParameter
+    climate_data_paths::String
+    zone_data_path::String
+    zone_names::Array
+    zones::Array{FarmZone}
+
+    function Basin(spec::Dict)
+
+        props = generate_params(spec["name"], spec["properties"])
+
+        basin = new()
+        basin.climate_options = props[:climate_options]
+        basin.zone_names = props[:zones]
+
+        basin.climate_data_paths = spec["climate_data"]
+        basin.zone_data_path = spec["zone_data"]
+
+        basin.zones = setup_zones(basin.zone_data_path, basin.zone_names)
+
+        return basin
+    end
+end
+
+
+function setup_zones(data_path::String, zone_names::Array)
+    zone_spec_dir::String = data_path
+    zone_specs::Dict{String, Dict} = load_yaml(zone_spec_dir)
+
+    return [create(FarmZone, z_spec) for (z_name, z_spec) in zone_specs if z_name in zone_names]
+end
