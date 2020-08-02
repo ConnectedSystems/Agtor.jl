14ac366ebdd8e2eebc29c661b3af5e24ddf8b256
ConnectedSystems <takuyai@gmail.com>
2020-08-02 17:28:24 +1000

    exporting new functions/macros

--- src/Agtor.jl
+++ src/Agtor.jl
@@ -27,7 +27,8 @@ AgUnion = Union{Int64, Float64, Agtor.AgParameter}
 
 export AgComponent, AgParameter, Infrastructure, Irrigation, Pump
 export WaterSource, Crop, FarmField, CropField, FarmZone, Manager, Climate, Basin
-export load_yaml, generate_params, create, run_timestep, subtotal_costs, total_costs
-export collect_results, min_max, param_values, param_info, AgUnion, add_prefix!, @def
+export load_yaml, generate_agparams, create, run_timestep, subtotal_costs, total_costs
+export collect_results, min_max, extract_values, param_info, extract_spec, AgUnion, add_prefix!, @def
+export @create, extract_agparams, collect_agparams!, collect_agparams
 
 end # module
