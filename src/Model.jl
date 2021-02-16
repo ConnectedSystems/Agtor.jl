89c7ffb88df180bff1ac699bac7dd324ed78de6d
ConnectedSystems <takuyai@gmail.com>
2021-02-16 19:18:28 +1100

    Add pre and post callbacks

--- src/Model.jl
+++ src/Model.jl
@@ -11,7 +11,11 @@ ts_func : Function, defining actions for a time step.
         `ts_func(manager, zone, idx, dt)`
 
         All operations must be in-place as changes will not propagate.
-callback_func : Function, defines additional actions for `zone` that occur 
+pre : Function, defines additional actions for `zone` that occur 
+        at end of time step `dt_i`:
+
+        `callback_func(zone, dt_i)`
+post : Function, defines additional actions for `zone` that occur 
                 at end of time step `dt_i`:
 
                 `callback_func(zone, dt_i)`
@@ -20,13 +24,20 @@ Returns
 ----------
 DataFrame : results
 """
-function run_model(zone, ts_func::Function, callback_func::Function)
+function run_model(zone, ts_func::Function; pre::Union{Function, Nothing}=nothing, post::Union{Function, Nothing}=nothing)
     
     time_sequence = zone.climate.time_steps
 
     @inbounds for (idx, dt_i) in enumerate(time_sequence)
+        if !isnothing(pre)
+            pre(zone, dt_i)
+        end
+
         ts_func(zone.manager, zone, idx, dt_i)
-        callback_func(zone, dt_i)
+
+        if !isnothing(post)
+            post(zone, dt_i)
+        end
     end
 
     return collect_results(zone)
