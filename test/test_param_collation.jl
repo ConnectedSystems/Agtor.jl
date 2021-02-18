34894fb195bcfbfec29fa7bded93b2fb4619ac18
ConnectedSystems <takuyai@gmail.com>
2021-02-18 17:04:01 +1100

    Add test to catch duplicate parameter entries

--- test/test_param_collation.jl
+++ test/test_param_collation.jl
@@ -1,12 +1,25 @@
-using Agtor, Dates, CSV
 using Test
 
-import Flatten
+using Agtor, Dates, CSV
 
 
 function setup_zone(data_dir::String="test/data/")
-    zone_spec_dir::String = "$(data_dir)zones/"
-    zone_specs::Dict{Symbol, Dict} = load_spec(zone_spec_dir)
+    zone_spec_dir::String = "$(data_dir)zones"
+    zone_params::Dict{Symbol, Dict} = load_spec(zone_spec_dir)
 
-    return [create(z_spec) for (z_name, z_spec) in zone_specs]
+    return create(zone_params[:Zone_1])
 end
+
+
+function test_no_duplicate_params()
+    zone = setup_zone()
+
+    all_params = collect_agparams!(zone)
+    param_names = all_params[:name]
+    uni_params = unique(param_names)
+
+    @test length(param_names) == length(uni_params)
+end
+
+    
+test_no_duplicate_params()
