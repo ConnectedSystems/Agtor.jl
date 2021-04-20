a20316ebff027c5c162aa4eab232cf092e486932
ConnectedSystems <takuyai@gmail.com>
2021-04-20 18:39:43 +1000

    Simplified parameter update example

--- examples/updating_agparams.jl
+++ examples/updating_agparams.jl
@@ -13,30 +13,18 @@ using Agtor
 # Windows:
 # $ set JULIA_NUM_THREADS=4 && julia dev.jl
 
-function setup_zone(data_dir::String="test/data/")
-    zone_spec_dir::String = "$(data_dir)zones/"
-    zone_specs::Dict{String, Dict} = load_yaml(zone_spec_dir)
-
-    zone_params = generate_agparams("", zone_specs["Zone_1"])
-
-    collated_specs::Array = []
-    agparams = collect_agparams!(zone_params, collated_specs; ignore=["crop_spec"])
-
-    climate_fn::String = "$(data_dir)climate/farm_climate_data.csv"
-    climate::Climate = load_climate(climate_fn)
-
-    return create(zone_params, climate), agparams
-end
-
 
 data_dir = "test/data/"
+zone_spec_dir::String = "$(data_dir)zones/"
+zone_params::Dict{String, Dict} = load_spec(zone_spec_dir)
 
-z1, collated = setup_zone(data_dir)
+z1 = create(zone_params[:Zone_1])
 
+# Grab some pre-generated parameter combinations
 samples = DataFrame!(CSV.File("test/data/scenarios/sampled_params.csv"))
 
+# Below we update the model with the sampled values
 
-# Here we update the model with the sampled values
 # The "value" field for each parameter should change.
 @info "before update" z1.fields[1].irrigation.efficiency
 
